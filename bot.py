94dec83b9ba32c64ecdeff9504a1c6b26a6770d4
Javier Lopez Garriga <101639484+yasjavi@users.noreply.github.com>
2024-02-02 15:56:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,37 @@
 import github
-import os
 import requests
 import base64
 
-# Authenticate with GitHub using a personal access token
-g = github.Github("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")
+# Autenticación con GitHub usando un token de acceso personal
+g = github.Github("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")
 
-# Get the repository
+# Obtener el repositorio
 repo = g.get_user().get_repo("repobotnet")
 
-# Define la URL del archivo .passwd en GitHub
+# Definir la URL del archivo a descargar
 url = 'https://raw.githubusercontent.com/yasjavi/repobotnet/main/.passwd'
 
-def download_passwd():
+def download_file():
     print('[!] Descargando .passwd')
     response = requests.get(url)
     if response.status_code == 200:
-        content = response.content.decode('utf-8')
-        with open(".passwd", 'w') as file:
+        content = response.content
+        with open(".passwd", 'wb') as file:
             file.write(content)
         print('[!] .passwd descargado con éxito.')
     else:
         print('[-] Fallo al descargar .passwd. Código de estado:', response.status_code)
 
-# Descarga .passwd y lo almacena localmente
-download_passwd()
+def upload_file():
+    print('[!] Subiendo .passwd al repositorio')
+    with open(".passwd", "rb") as file:
+        # Crear un nuevo archivo en el repositorio
+        repo.create_file(".passwd", "Actualizando .passwd en el repositorio", file.read())
+    print('[!] .passwd cargado con éxito en el repositorio.')
 
-# Abre el archivo .passwd en modo binario
-with open(".passwd", "rb") as f:
-    # Crea un nuevo archivo en el repositorio
-    repo.create_file(".passwd", "Actualizando .passwd en el repositorio", f.read())
+# Descargar el archivo
+download_file()
+
+# Subir el archivo al repositorio
+upload_file()
+
